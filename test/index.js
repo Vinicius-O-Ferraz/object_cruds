50aa63f2198d0f037b30af726d6c2837aaa5b790
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2026-09-08 19:34:29 -0300

    feat: adicionndo validação da placa

--- test/index.js
+++ test/index.js
@@ -6,6 +6,9 @@ const supabase = createClient(
     process.env.SUPABASE_ANON_KEY
 );
 
+/*
+    Métodos para consultar e inserir veículos na tabela 'veiculo' do Supabase.
+*/
 
 async function consultarVeiculos() {
     const { data, error } = await supabase
@@ -20,11 +23,39 @@ async function consultarVeiculos() {
     console.table(data);
 }
 
+function validarPlaca(placa) {
+
+    if (typeof placa !== 'string') {
+        throw new Error('A placa deve ser uma string.');
+    }
+
+    // Remove todos os hífens
+    const placaLimpa = placa.replace(/-/g, '')
+   
+    // Remove espaços no início e no final
+    const placaNormalizada = placa
+    .replace(/-/g, '')
+    .replace(/\s/g, '')
+    .trim();
+
+    // Valida o tamanho
+    const tamanhosValidos = [7, 8, 11];
+
+    if (!tamanhosValidos.includes(placaNormalizada.length)) {
+        throw new Error(
+            `Placa inválida: "${placa}". ` +
+            `A placa deve possuir 7, 8 ou 11 caracteres após a remoção dos hífens.`
+        );
+    }
+
+    return placaNormalizada;
+}
+
 async function inserirVeiculos(placa) {
      const { data, error } = await supabase
         .from('veiculo')
         .insert({
-            placa: placa
+            placa: validarPlaca(placa)
         })
         .select();
 
@@ -38,10 +69,39 @@ async function inserirVeiculos(placa) {
     console.table(data);
 }
 
+async function formatarVeiculos(placa) {
+
+}
+
+/*
+    Casos de teste para inserir veículos com diferentes formatos de placa e consultar os veículos inseridos.
+*/
 
 
+// var placa = 'BCD1235';
+// inserirVeiculos(placa);
 
-var placa = 'BCD1234';
-inserirVeiculos(placa);
+// var placa = 'ABA1235';
+// inserirVeiculos(placa);
+
+// var placa = 'BABA1235';
+// inserirVeiculos(placa);
+
+// var placa = 'MSCU-123456-9';
+// inserirVeiculos(placa);
+
+// var placa = 'BCD-1234';
+// inserirVeiculos(placa);
+
+// var placa = 'ABA-1234';
+// inserirVeiculos(placa);
+
+// var placa = 'BABA-1234';
+// inserirVeiculos(placa);
+
+// var placa = 'MSCU 123456 8';
+// inserirVeiculos(placa);
+
+
 
 consultarVeiculos();
